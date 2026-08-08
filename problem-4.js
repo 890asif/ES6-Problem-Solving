8c6235a32ed2910ebf1ce752d92f4828ccc1d0f6
890asif <192508892+890asif@users.noreply.github.com>
2026-08-09 00:05:20 +0600

    total pice after discount

---
+++ problem-4.js
@@ -0,0 +1,19 @@
+const findtotalPriceAfterDiscount = (products, category)=>{
+
+    const totalPrice = products.filter(product => product.category === category ).map(product => product.price * 0.85).reduce((total, price)=>{
+        
+        return total + price 
+    },0)
+
+    return totalPrice
+}
+
+const products = [
+  { name: "Mouse", category: "electronics", price: 1000 },
+  { name: "Keyboard", category: "electronics", price: 2000 },
+  { name: "Book", category: "stationery", price: 500 }
+];
+
+const category = "electronics";
+
+console.log(findtotalPriceAfterDiscount(products, category))
